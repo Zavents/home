7078e8253a15f69fc1094a6d2b1655b6d81b9848
zaven <pku.mca16@gmail.com>
2024-09-28 09:18:09 +0700

    asd

--- js/main.js
+++ js/main.js
@@ -1,65 +1,3 @@
-
-AOS.init({
-})
-window.addEventListener("load", () => {
-  clock();
-  function clock() {
-    const today = new Date();
-
-    // get time components
-    const hours = today.getHours();
-    const minutes = today.getMinutes();
-    const seconds = today.getSeconds();
-
-    //add '0' to hour, minute & second when they are less 10
-    const hour = hours < 10 ? "0" + hours : hours;
-    const minute = minutes < 10 ? "0" + minutes : minutes;
-    const second = seconds < 10 ? "0" + seconds : seconds;
-
-    //make clock a 12-hour time clock
-    const hourTime = hour > 12 ? hour - 12 : hour;
-
-    // if (hour === 0) {
-    //   hour = 12;
-    // }
-    //assigning 'am' or 'pm' to indicate time of the day
-    const ampm = hour < 12 ? "AM" : "PM";
-
-    // get date components
-    const month = today.getMonth();
-    const year = today.getFullYear();
-    const day = today.getDate();
-
-    //declaring a list of all months in  a year
-    const monthList = [
-      "Januari",
-      "Februari",
-      "Maret",
-      "April",
-      "Mei",
-      "Juni",
-      "Juli",
-      "Augustus",
-      "September",
-      "oktober",
-      "November",
-      "Desember"
-    ];
-
-    //get current date and time
-    const date = monthList[month] + " " + day + ", " + year;
-    const time = hourTime + ":" + minute + ":" + second + ampm;
-
-    //combine current date and time
-    const dateTime = date + "  " + time;
-
-    //print current date and time to the DOM
-    document.getElementById("date-time").innerHTML = dateTime;
-    setTimeout(clock, 1000);
-  }
-});
-
-
 document.getElementById('youtubeButton').addEventListener('click', function() {
   Swal.fire({
     title: 'Navigation',
@@ -100,3 +38,7 @@ document.getElementById('youtubeButton').addEventListener('click', function() {
     `
   });
 });
+AOS.init({
+})
+
+
